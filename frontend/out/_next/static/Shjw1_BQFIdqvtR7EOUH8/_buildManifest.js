5e236d29c02aef795f73fb7aeadb3862463bc36a
MacBook Air M1 <macbookairm1@MacBooks-MacBook-Air.local>
2026-09-11 18:48:29 +0200

    build(frontend): publish dashboard debt summary

--- frontend/out/_next/static/Shjw1_BQFIdqvtR7EOUH8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-07fdd360ad4b7231.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
